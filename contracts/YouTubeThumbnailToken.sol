4ad9dd87c7672f1546f20ccbb4270ee0ab09d7f3
17wook2 <wook@wook2.localdomain>
2021-05-07 04:04:38 +0900

    Token creation login done

--- contracts/YouTubeThumbnailToken.sol
+++ contracts/YouTubeThumbnailToken.sol
@@ -11,22 +11,30 @@ contract YouTubeThumbnailToken is ERC721Full{
 
   mapping (uint256 => YouTubeThumbnail) YouTubeThumbnails;
   mapping (string => uint256) videoIdsCreated;
-  constructor(string memory name, string memory symbol) ERC721Full(name, symbol) public {
+  constructor(string memory name, string memory symbol) ERC721Full(name, symbol) public {}
+
       function mintYTT(
         string memory _videoId,
         string memory _author,
         string memory _dateCreated,
         string memory _tokenURI
       )
-        public
+      public
       {
         require(videoIdsCreated[_videoId] == 0, "videoId has already been created");
         uint256 tokenId = totalSupply().add(1);
-        youTubeThumbnails[tokenId] = YouTubeThumbnail(_author, _dateCreated);
+        YouTubeThumbnails[tokenId] = YouTubeThumbnail(_author, _dateCreated);
         videoIdsCreated[_videoId] = tokenId; 
 
         _mint(msg.sender, tokenId);
-        _setTokenURI(toeknId, _tokenURI);
+        _setTokenURI(tokenId, _tokenURI);
       }
-  }
+
+      function getYTT(uint256 _tokenId) public view returns (string memory, string memory) {
+        return (YouTubeThumbnails[_tokenId].author, YouTubeThumbnails[_tokenId].dateCreated);
+      }
+
+      function isTokenAlreadyCreated(string memory _videoId) public view returns (bool) {
+        return videoIdsCreated[_videoId] != 0 ? true : false;
+      }
 } 
